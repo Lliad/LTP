b71b145b9296b56682408a7b5ccc86e30f404caa
Lliad <865194452@qq.com>
2020-03-27 13:43:49 +0800

    Update sdr.h

--- sdr.h
+++ sdr.h
@@ -4,5 +4,4 @@ extern int		sdr_begin_xn(Sdr sdr);
 
 extern int		sdr_end_xn(Sdr sdr);
 
-extern Object		sdr_insert(Sdr sdr, char *from, 
-								size_t size);
+extern Object		sdr_insert(Sdr sdr, char *from, size_t size);
