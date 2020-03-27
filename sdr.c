d38aa9de30357cdae54f3641b4f8bfe439c94947
Lliad <865194452@qq.com>
2020-03-27 13:44:53 +0800

    Update sdr.c

--- sdr.c
+++ sdr.c
@@ -8,53 +8,16 @@
 
 int	sdr_begin_xn(Sdr sdrv)
 {
-	CHKZERO(sdrv);
-	if (takeSdr(sdrv->sdr) < 0)
-	{
-		return 0;	/*	Failed to begin transaction.	*/
-	}
 
-	sdrv->modified = 0;
-	return 1;		/*	Began transaction.		*/
 }
 
 int	sdr_end_xn(Sdr sdrv)
 {
-	SdrState	*sdr;/* def in sdrP.h */
 
-	CHKERR(sdrv);
-	sdr = sdrv->sdr;
-	if (sdr_in_xn(sdrv))
-	{
-		sdr->xnDepth--;
-		if (sdr->xnDepth == 0)
-		{
-			terminateXn(sdrv);
-		}
-
-		return 0;
-	}
-
-	return -1;
 }
 
 Object	sdr_insert(Sdr sdrv, char *from,
 		size_t size)
 {
-	Object	obj;
 
-	if (!(sdr_in_xn(sdrv)))
-	{
-		oK(_iEnd(file, line, _notInXnMsg()));
-		return 0;
-	}
-
-	joinTrace(sdrv, file, line);
-	obj = _sdrmalloc(sdrv, size);
-	if (obj)
-	{
-		_sdrput(file, line, sdrv, (Address) obj, from, size, SystemPut);
-	}
-
-	return obj;
 }
